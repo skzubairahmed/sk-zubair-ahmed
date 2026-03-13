fa73c7d83572d8b21f371b1114263fb42a516735
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-03-13 08:38:02 +0530

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <main className="h-min-screen bg-slate-950 w-full p-2 flex flex-col justify-center items-center">
-      <div className="p-4 rounded-lg bg-slate-850 text-amber-450">
-        Welcome, this site is still under construction!
+    <div className="h-screen bg-slate-950 p-2 flex flex-col justify-center items-center">
+      <div className="p-4 rounded-lg bg-black">
+        <p className="text text-emerald-500 terminal-text font-mono antialiased">
+          Welcome, this site is still under contruction.
+        </p>
       </div>
-    </main>
+    </div>
   );
 }
